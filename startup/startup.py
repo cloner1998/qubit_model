f3cde084c97d83f2e6f67ecc9d172ee1b710c122
sajjad <sajjadyami77@gmail.com>
2024-08-19 13:06:50 +0330

    [fix] minor fix and add init plot

--- startup/startup.py
+++ startup/startup.py
@@ -19,16 +19,31 @@ t_list = np.linspace(1, 10, 100)
 mutual_information_list = []
 spins_spin_correlation_list = []
 
-for t in t_list:
-    # Apply perturbation in the past
-    psi_t = time_evolution.time_evolution(psi_0, hamiltonian, -t)
-    psi_t = perturbation * psi_t
 
-    # Evolve forward
-    psi_t = time_evolution.time_evolution(psi_t, hamiltonian, t)
+def mutual_info_list():
+    for t in t_list:
+        # Apply perturbation in the past
+        psi_t = time_evolution.time_evolution(psi_0, hamiltonian, -t)
+        psi_t = perturbation * psi_t
 
-    # Compute observables
-    density_matrix = psi_t * psi_t.dag()
-    mutual_information_list.append(mutual_information.mutual_information(density_matrix, qubits))
-    spins_spin_correlation_list.append(spins_spin_correlation.spin_spin_correlation(psi_t, qubits))
+        # Evolve forward
+        psi_t = time_evolution.time_evolution(psi_t, hamiltonian, t)
 
+        # Compute observables
+        density_matrix = psi_t * psi_t.dag()
+        mutual_information_list.append(mutual_information.mutual_information(density_matrix, qubits))
+    return mutual_information_list
+
+
+def spin_spin_info_list():
+    for t in t_list:
+        # Apply perturbation in the past
+        psi_t = time_evolution.time_evolution(psi_0, hamiltonian, -t)
+        psi_t = perturbation * psi_t
+
+        # Evolve forward
+        psi_t = time_evolution.time_evolution(psi_t, hamiltonian, t)
+
+        # Compute observables
+        spins_spin_correlation_list.append(spins_spin_correlation.spin_spin_correlation(psi_t, qubits))
+    return spins_spin_correlation_list
